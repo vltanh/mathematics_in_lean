21bb648a0e820d3bc16908c1d194a8d822cc4a35
The-Anh Vu-Le <vltanh@illinois.edu>
2024-09-15 18:05:07 -0500

    finish C04/S02

--- my_copy/C04_Sets_and_Functions/S02_Functions.lean
+++ my_copy/C04_Sets_and_Functions/S02_Functions.lean
@@ -34,66 +34,156 @@ example : s ⊆ f ⁻¹' (f '' s) := by
   use x, xs
 
 example : f '' s ⊆ v ↔ s ⊆ f ⁻¹' v := by
-  sorry
+  constructor
+  · intro hfsv _ hs
+    exact hfsv (mem_image_of_mem _ hs)
+  · intro hsfinvv _ hyfs
+    rcases hyfs with ⟨x, hxs, hfxy⟩
+    rw [← hfxy]
+    apply hsfinvv hxs
 
 example (h : Injective f) : f ⁻¹' (f '' s) ⊆ s := by
-  sorry
+  rintro x ⟨y, hys, hfyfx⟩
+  rw [← (h hfyfx)]
+  exact hys
 
 example : f '' (f ⁻¹' u) ⊆ u := by
-  sorry
+  rintro y ⟨x, hxfinvu, hfxy⟩
+  rw [← hfxy]
+  exact hxfinvu
 
 example (h : Surjective f) : u ⊆ f '' (f ⁻¹' u) := by
-  sorry
+  intro y hyu
+  rcases (h y) with ⟨x, hfxy⟩
+  rw [← hfxy] at hyu
+  exact ⟨x, hyu, hfxy⟩
 
 example (h : s ⊆ t) : f '' s ⊆ f '' t := by
-  sorry
+  rintro y ⟨x, hxs, hfxy⟩
+  use x
+  exact ⟨h hxs, hfxy⟩
 
 example (h : u ⊆ v) : f ⁻¹' u ⊆ f ⁻¹' v := by
-  sorry
+  intro x hxfinvu
+  exact h hxfinvu
 
 example : f ⁻¹' (u ∪ v) = f ⁻¹' u ∪ f ⁻¹' v := by
-  sorry
+  ext
+  rfl
 
 example : f '' (s ∩ t) ⊆ f '' s ∩ f '' t := by
-  sorry
+  rintro _ ⟨x, hxst, rfl⟩
+  exact ⟨mem_image_of_mem _ hxst.left, mem_image_of_mem _ hxst.right⟩
 
 example (h : Injective f) : f '' s ∩ f '' t ⊆ f '' (s ∩ t) := by
-  sorry
+  rintro y ⟨⟨x, hxs, hfxy⟩, ⟨x', hx't, hfx'y⟩⟩
+  rw [← hfx'y] at hfxy
+  rw [← h hfxy] at hx't
+  use x
+  constructor
+  · exact ⟨hxs, hx't⟩
+  · rw [← hfx'y]
+    exact hfxy
 
 example : f '' s \ f '' t ⊆ f '' (s \ t) := by
-  sorry
+  rintro y ⟨⟨x, hxs, hfxy⟩, hynft⟩
+  use x
+  constructor
+  · constructor
+    · exact hxs
+    · intro hxt
+      apply hynft
+      use x
+  · exact hfxy
 
 example : f ⁻¹' u \ f ⁻¹' v ⊆ f ⁻¹' (u \ v) := by
-  sorry
+  exact fun _ => id
 
 example : f '' s ∩ v = f '' (s ∩ f ⁻¹' v) := by
-  sorry
+  apply Subset.antisymm
+  · rintro y ⟨⟨x, hxs, rfl⟩, hyv⟩
+    use x, ⟨hxs, hyv⟩
+  · rintro y ⟨x, ⟨hxs, hxfinvv⟩, rfl⟩
+    exact ⟨mem_image_of_mem f hxs, hxfinvv⟩
 
 example : f '' (s ∩ f ⁻¹' u) ⊆ f '' s ∩ u := by
-  sorry
+  rintro y ⟨x, ⟨hxs, hxfinvu⟩, rfl⟩
+  exact ⟨mem_image_of_mem _ hxs, hxfinvu⟩
 
 example : s ∩ f ⁻¹' u ⊆ f ⁻¹' (f '' s ∩ u) := by
-  sorry
+  rintro x ⟨hxs, hfinvu⟩
+  exact ⟨mem_image_of_mem _ hxs, hfinvu⟩
 
 example : s ∪ f ⁻¹' u ⊆ f ⁻¹' (f '' s ∪ u) := by
-  sorry
+  rintro x (hxs | hxfinvu)
+  · exact Or.inl (mem_image_of_mem _ hxs)
+  · exact Or.inr hxfinvu
 
 variable {I : Type*} (A : I → Set α) (B : I → Set β)
 
 example : (f '' ⋃ i, A i) = ⋃ i, f '' A i := by
-  sorry
+  apply Subset.antisymm
+  · rintro y ⟨x, hxuAi, rfl⟩
+    rw [mem_iUnion] at *
+    rcases hxuAi with ⟨i, hAi⟩
+    use i
+    exact mem_image_of_mem f hAi
+  · rintro y hyufAi
+    rw [mem_iUnion] at hyufAi
+    rcases hyufAi with ⟨i, ⟨x, hxAi, rfl⟩⟩
+    use x
+    constructor
+    · rw [mem_iUnion]
+      use i
+    · rfl
 
 example : (f '' ⋂ i, A i) ⊆ ⋂ i, f '' A i := by
-  sorry
+  rintro y ⟨x, hxiAi, rfl⟩
+  rw [mem_iInter] at *
+  intro i
+  exact mem_image_of_mem _ (hxiAi i)
 
 example (i : I) (injf : Injective f) : (⋂ i, f '' A i) ⊆ f '' ⋂ i, A i := by
-  sorry
+  rintro y hyifAi
+  rw [mem_iInter] at hyifAi
+  rcases (hyifAi i) with ⟨x, hxAi, rfl⟩
+  use x
+  constructor
+  · rw [mem_iInter]
+    intro j
+    rcases (hyifAi j) with ⟨x', hx'Aj, hfx'fx⟩
+    have hx'x := injf hfx'fx
+    rw [hx'x] at hx'Aj
+    exact hx'Aj
+  · rfl
 
 example : (f ⁻¹' ⋃ i, B i) = ⋃ i, f ⁻¹' B i := by
-  sorry
+  apply Subset.antisymm
+  · rintro x hxfinvuBi
+    have hfxuBi : f x ∈ ⋃ i, B i := hxfinvuBi
+    rw [mem_iUnion] at *
+    rcases hfxuBi with ⟨i, hfxBi⟩
+    use i
+    exact hfxBi
+  · rintro x hxufinvBi
+    show f x ∈ ⋃ i, B i
+    rw [mem_iUnion] at *
+    rcases hxufinvBi with ⟨i, hxfinvBi⟩
+    use i
+    exact hxfinvBi
 
 example : (f ⁻¹' ⋂ i, B i) = ⋂ i, f ⁻¹' B i := by
-  sorry
+  apply Subset.antisymm
+  · rintro x hxfinviBi
+    have hfxiBi : f x ∈ ⋂ i, B i := hxfinviBi
+    rw [mem_iInter] at *
+    intro i
+    exact hfxiBi i
+  · rintro x hxifinvBi
+    show f x ∈ ⋂ i, B i
+    rw [mem_iInter] at *
+    intro i
+    exact hxifinvBi i
 
 example : InjOn f s ↔ ∀ x₁ ∈ s, ∀ x₂ ∈ s, f x₁ = f x₂ → x₁ = x₂ :=
   Iff.refl _
@@ -123,16 +213,37 @@ example : range exp = { y | y > 0 } := by
   rw [exp_log ypos]
 
 example : InjOn sqrt { x | x ≥ 0 } := by
-  sorry
+  intro x1 x1pos x2 x2pos hsqrtx1sqrtx2
+  calc
+    x1 = (sqrt x1) ^ 2 := by rw [sq_sqrt x1pos]
+    _ = (sqrt x2) ^ 2 := by rw [hsqrtx1sqrtx2]
+    _ = x2 := by rw [sq_sqrt x2pos]
 
 example : InjOn (fun x ↦ x ^ 2) { x : ℝ | x ≥ 0 } := by
-  sorry
+  intro x1 x1pos x2 x2pos hsqx1sqx2
+  dsimp at hsqx1sqx2
+  calc
+    x1 = sqrt (x1 ^ 2) := by rw [sqrt_sq x1pos]
+    _ = sqrt (x2 ^ 2) := by rw [hsqx1sqx2]
+    _ = x2 := by rw [sqrt_sq x2pos]
 
 example : sqrt '' { x | x ≥ 0 } = { y | y ≥ 0 } := by
-  sorry
+  apply Subset.antisymm
+  · rintro y ⟨x, hxpos, rfl⟩
+    exact sqrt_nonneg x
+  · rintro y hypos
+    use y ^ 2
+    exact ⟨sq_nonneg y, sqrt_sq hypos⟩
 
 example : (range fun x ↦ x ^ 2) = { y : ℝ | y ≥ 0 } := by
-  sorry
+  apply Subset.antisymm
+  · intro y ⟨x, hsqxy⟩
+    rw [← hsqxy]
+    exact sq_nonneg x
+  · intro y hypos
+    use sqrt y
+    dsimp
+    rw [sq_sqrt hypos]
 
 end
 
@@ -163,11 +274,29 @@ variable (f : α → β)
 
 open Function
 
-example : Injective f ↔ LeftInverse (inverse f) f :=
-  sorry
+example : Injective f ↔ LeftInverse (inverse f) f := by
+  constructor
+  · intro hinjf
+    intro x
+    apply hinjf
+    apply inverse_spec (f x)
+    use x
+  · intro hlinvinvff
+    intro x1 x2 hfx1fx2
+    calc
+      x1 = inverse f (f x1) := by rw [hlinvinvff]
+      _ = inverse f (f x2) := by rw [hfx1fx2]
+      _ = x2 := by rw [hlinvinvff]
 
-example : Surjective f ↔ RightInverse (inverse f) f :=
-  sorry
+example : Surjective f ↔ RightInverse (inverse f) f := by
+  constructor
+  · intro hsurf
+    intro y
+    exact inverse_spec y (hsurf y)
+  · intro hrinvinvff
+    intro y
+    use inverse f y
+    exact hrinvinvff y
 
 end
 
@@ -183,10 +312,8 @@ theorem Cantor : ∀ f : α → Set α, ¬Surjective f := by
     intro h'
     have : j ∉ f j := by rwa [h] at h'
     contradiction
-  have h₂ : j ∈ S
-  sorry
-  have h₃ : j ∉ S
-  sorry
+  have h₂ : j ∈ S := h₁
+  have h₃ : j ∉ S := by rwa [h] at h₁
   contradiction
 
 -- COMMENTS: TODO: improve this
