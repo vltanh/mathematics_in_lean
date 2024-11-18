c13d106e38b4591d19e7a0a463a3d13ad720ff8b
The-Anh Vu-Le <vltanh@illinois.edu>
2024-11-18 00:24:25 -0600

    finish C07/S03

--- my_copy/C07_Hierarchies/S03_Subobjects.lean
+++ my_copy/C07_Hierarchies/S03_Subobjects.lean
@@ -52,8 +52,36 @@ instance [Monoid M] : SubmonoidClass₁ (Submonoid₁ M) M where
   mul_mem := Submonoid₁.mul_mem
   one_mem := Submonoid₁.one_mem
 
+-- Define a Subgroup₁ structure
+@[ext]
+structure Subgroup₁ (G : Type) [Group G] extends Submonoid₁ G where
+  inv_mem {a} : a ∈ carrier → a⁻¹ ∈ carrier
 
-instance [Monoid M] : Inf (Submonoid₁ M) :=
+-- Endow it with a SetLike instance
+instance [Group G] : SetLike (Subgroup₁ G) G where
+  coe := fun H ↦ H.toSubmonoid₁.carrier
+  coe_injective' _ _ := Subgroup₁.ext
+--               a SubmonoidClass₁ instance
+instance [Group G] : SubmonoidClass₁ (Subgroup₁ G) G where
+  mul_mem := fun H ↦ H.toSubmonoid₁.mul_mem
+  one_mem := fun H ↦ H.toSubmonoid₁.one_mem
+
+-- Put a Group instance on the subtype associated to a Subgroup₁
+instance [Group G] (H : Subgroup₁ G) : Group H :=
+{ SubMonoid₁Monoid H.toSubmonoid₁ with
+  inv := fun x ↦ ⟨x⁻¹, H.inv_mem x.property⟩
+  inv_mul_cancel := fun x ↦ SetCoe.ext (inv_mul_cancel (x : G)) }
+
+-- Define a SubgroupClass₁ class
+class SubgroupClass₁ (S : Type) (G : Type) [Group G] [SetLike S G]
+    extends SubmonoidClass₁ S G  : Prop where
+  inv_mem : ∀ (s : S) {a : G}, a ∈ s → a⁻¹ ∈ s
+
+instance [Group G] : SubgroupClass₁ (Subgroup₁ G) G where
+  inv_mem := Subgroup₁.inv_mem
+
+-- Inf is deprecated, replaced by Min
+instance [Monoid M] : Min (Submonoid₁ M) :=
   ⟨fun S₁ S₂ ↦
     { carrier := S₁ ∩ S₂
       one_mem := ⟨S₁.one_mem, S₂.one_mem⟩
@@ -69,7 +97,16 @@ def Submonoid.Setoid [CommMonoid M] (N : Submonoid M) : Setoid M  where
     refl := fun x ↦ ⟨1, N.one_mem, 1, N.one_mem, rfl⟩
     symm := fun ⟨w, hw, z, hz, h⟩ ↦ ⟨z, hz, w, hw, h.symm⟩
     trans := by
-      sorry
+      rintro x y z ⟨w, hwN, t, htN, h⟩ ⟨w', hw'N, t', ht'N, h'⟩
+      use w * w'
+      constructor
+      · exact N.mul_mem hwN hw'N
+      · use t * t'
+        constructor
+        · exact N.mul_mem htN ht'N
+        · rw [← mul_assoc, mul_comm t t', ← mul_assoc]
+          rw [h, ← h']
+          rw [mul_assoc, mul_comm t w', ← mul_assoc]
   }
 
 instance [CommMonoid M] : HasQuotient M (Submonoid M) where
@@ -79,12 +116,43 @@ def QuotientMonoid.mk [CommMonoid M] (N : Submonoid M) : M → M ⧸ N := Quotie
 
 instance [CommMonoid M] (N : Submonoid M) : Monoid (M ⧸ N) where
   mul := Quotient.map₂' (· * ·) (by
-      sorry
+      rintro a b ⟨w, hwN, z, hzN, h⟩ c d ⟨w', hw'N, z', hz'N, h'⟩
+      dsimp
+      use w * w'
+      constructor
+      · exact N.mul_mem hwN hw'N
+      · use z * z'
+        constructor
+        · exact N.mul_mem hzN hz'N
+        · calc
+            a * c * (w * w') = a * (c * (w * w')) := by rw [mul_assoc]
+            _ = a * (c * w * w') := by rw [mul_assoc]
+            _ = a * (w * c * w') := by rw [mul_comm c w]
+            _ = a * (w * (c * w')) := by rw [mul_assoc]
+            _ = a * w * (c * w') := by rw [mul_assoc]
+            _ = b * z * (d * z') := by rw [h, h']
+            _ = b * (z * (d * z')) := by rw [mul_assoc]
+            _ = b * (z * d * z') := by rw [mul_assoc]
+            _ = b * (d * z * z') := by rw [mul_comm z d]
+            _ = b * (d * (z * z')) := by rw [mul_assoc]
+            _ = b * d * (z * z') := by rw [mul_assoc]
         )
   mul_assoc := by
-      sorry
+      rintro ⟨a⟩ ⟨b⟩ ⟨c⟩
+      apply Quotient.sound
+      dsimp
+      rw [mul_assoc]
+      apply @Setoid.refl M N.Setoid
   one := QuotientMonoid.mk N 1
   one_mul := by
-      sorry
+      rintro ⟨a⟩
+      apply Quotient.sound
+      dsimp
+      rw [one_mul]
+      apply @Setoid.refl M N.Setoid
   mul_one := by
-      sorry
+      rintro ⟨a⟩
+      apply Quotient.sound
+      dsimp
+      rw [mul_one]
+      apply @Setoid.refl M N.Setoid
